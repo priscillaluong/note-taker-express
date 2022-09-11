b5e0521716e449ce229bf39efa80b9620cba5f3a
P Luong <p-luong@live.co.uk>
2022-09-11 13:04:28 +0100

    amended get requests in server.js

--- server.js
+++ server.js
@@ -1,31 +1,51 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 // const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+// const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
 // Import custom middleware, "cLog"
-app.use(clog);
+// app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+//app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+app.get('/api/notes/:id', (req, res) => {
+  const dbNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(dbNotes[Number(req.params.id)]);
+});
+
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+app.post('/api/notes', (req, res) => {
+    const dbNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let note = req.body;
+    let id = dbNotes.length.toString();
+    note.id = id;
+    dbNotes.push(note);
+    console.log(dbNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbNotes));
+    res.json(dbNotes);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
